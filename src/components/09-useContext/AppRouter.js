02e8aabb6aa4797034e7128865d39a7899001066
Manu Alvarez <alvarezmanuelignacio@gmail.com>
2021-12-15 20:18:26 -0300

    initial commit

---
+++ src/components/09-useContext/AppRouter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+    
+  } from 'react-router-dom';
+
+import { NavBar } from './NavBar';
+
+import { AboutScreen } from './AboutScreen';
+import { LoginScreen } from './LoginScreen';
+import { HomeScreen } from './HomeScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                
+                <NavBar />
+
+                <div className="container">
+                    <Routes>
+                    <Route path="/about" element={<AboutScreen/>}/>
+                    <Route path="/loginscreen" element={<LoginScreen/>}/>
+                    <Route path="/" element={<HomeScreen/>}/>
+                </Routes>
+                </div>
+            </div>
+        </Router>
+    )
+}
